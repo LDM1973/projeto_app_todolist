bbc511e593026d0710d50821070ca01c3aba7880
Leonardo Munarolo <leonardodm510@gmail.com>
2023-06-27 17:45:27 -0300

    Icone Maskable adicionado

---
+++ app/src/main/java/io/github/ldm1973/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ldm1973.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
